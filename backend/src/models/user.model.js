70c5b9db4ee07575889b55ec48fd8ef97e1a477a
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-03-31 18:00:24 +0530

    Write Mongoose custom methods for token generation and password validation in User document

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -13,7 +14,7 @@ const userSchema = new mongoose.Schema(
       unique: true,
       trim: true,
       required: true,
-      lowercase:true,
+      lowercase: true,
     },
     password: {
       type: String,
@@ -53,10 +54,61 @@ userSchema.pre("save", async function () {
     this.password = await bcrypt.hash(this.password.trim(), 10);
   } catch (error) {
     console.error(`Password Hashing Failed | ${error}`);
-    throw error;
+    throw error; // throw error so that it is catched by the asyncHandler error-block
   }
 });
 
-const User = mongoose.model("User", userSchema);
+/*
+  Business logic for generating access/refresh tokens & possword validation are incorporated in form of mongoose custom method. This is because these methods are exclusive to the "User" documents only (tokens need to be generated for users only; similar for password validation).
+*/
+// Mongoose custom method: Access Token generation
+userSchema.methods.accessTokenGenerator = () => {
+  try {
+    const accessToken = jwt.sign(
+      {
+        email: this.email,
+        fullname: this.fullname,
+      },
+      process.env.ACCESS_TOKEN_KEY,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      }
+    );
+    return accessToken;
+  } catch (error) {
+    console.error(`Access Token generation failed | ${error}`);
+    throw error; // throw error so that it is catched by the asyncHandler error-block
+  }
+};
 
-export default User;
+// Mongoose custom method: Refresh Token generation
+userSchema.methods.refreshTokenGenerator = () => {
+  try {
+    const refreshToken = jwt.sign(
+      {
+        email: this.email,
+        fullname: this.fullname,
+      },
+      process.env.REFRESH_TOKEN_KEY,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+      }
+    );
+    return refreshToken;
+  } catch (error) {
+    console.error(`Refresh Token generation failed | ${error}`);
+    throw error; // throw error so that it is catched by the asyncHandler error-block
+  }
+};
+
+// Mongoose custom method: Password validation
+userSchema.methods.validatePassword = async (password) => {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    console.error(`Password validation failed | ${error}`);
+    throw error; // throw error so that it is catched by the asyncHandler error-block
+  }
+};
+
+export default User = mongoose.model("User", userSchema);
